e31b8c1a4f1cbf728c691e47e39f34f80ebfc6c5
bee-joo <bukin-danila@yandex.ru>
2022-08-09 23:45:02 +0300

    Add queries

--- src/main/kotlin/ru/theatrebel/repository/PlayWriterRelationRepository.kt
+++ src/main/kotlin/ru/theatrebel/repository/PlayWriterRelationRepository.kt
@@ -7,8 +7,9 @@ import java.util.UUID
 
 interface PlayWriterRelationRepository : CrudRepository<PlayWriterRelation, UUID> {
 
-    @Query(value = "SELECT * FROM plays_writers WHERE play_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT pw FROM PlayWriterRelation pw WHERE pw.playId = ?1")
     fun getAllByPlayId(id: Long): Set<PlayWriterRelation>
 
+    @Query(value = "SELECT pw FROM PlayWriterRelation pw WHERE pw.writerId = ?1")
     fun getAllByWriterId(id: Long): Set<PlayWriterRelation>
 }
