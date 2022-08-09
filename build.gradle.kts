5ecb42f17f427cfb7977b1f2b2c9b4d3fcc2011d
bee-joo <bukin-danila@yandex.ru>
2022-08-09 23:44:21 +0300

    Postgres instead of H2

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,9 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    // implementation("org.liquibase:liquibase-core")
-    runtimeOnly("com.h2database:h2")
+    implementation("org.liquibase:liquibase-core")
+    // runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
