60abd55f16c1db0bb5b43752b29af513e39144ae
bee-joo <bukin-danila@yandex.ru>
2022-08-10 22:31:31 +0300

    Catch exceptions and change ResponseEntity to ResponseObject

--- src/main/kotlin/ru/theatrebel/service/PlayService.kt
+++ src/main/kotlin/ru/theatrebel/service/PlayService.kt
@@ -2,7 +2,6 @@ package ru.theatrebel.service
 
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import ru.theatrebel.dto.PlayDto
 import ru.theatrebel.dto.toEntity
@@ -10,59 +9,84 @@ import ru.theatrebel.entity.Play
 import ru.theatrebel.entity.PlayWriterRelation
 import ru.theatrebel.entity.toView
 import ru.theatrebel.entity.update
+import ru.theatrebel.exception.NotFoundException
+import ru.theatrebel.exception.ValidateNullException
 import ru.theatrebel.repository.PlayRepository
 import ru.theatrebel.repository.PlayWriterRelationRepository
 import ru.theatrebel.repository.WriterRepository
 import ru.theatrebel.view.PlayView
+import ru.theatrebel.view.ResponseObject
+import ru.theatrebel.view.WriterView
 
 @Service
 class PlayService(private val playRepository: PlayRepository,
                   private val writerRepository: WriterRepository,
                   private val playWriterRelationRepository: PlayWriterRelationRepository) {
 
-    fun addPlay(playDto: PlayDto): ResponseEntity<Play> {
+    fun addPlay(playDto: PlayDto): ResponseObject<Play> {
         if (playDto.writerIds == null || !playDto.writerIds.all { writerRepository.existsById(it) }) {
-            return ResponseEntity(HttpStatus.BAD_REQUEST)
+            throw ValidateNullException("No writers found")
         }
 
-        val play = playRepository.save(playDto.toEntity())
+        val play = try {
+            playRepository.save(playDto.toEntity())
+        } catch (e: ValidateNullException) {
+            throw e
+        }
 
         playWriterRelationRepository.saveAll(
             playDto.writerIds.map { PlayWriterRelation(play.id!!, it) }
         )
 
-        return ResponseEntity.ok(play)
+        return ResponseObject(play, HttpStatus.OK.value())
     }
 
-    fun getPlay(id: Long): ResponseEntity<PlayView> {
+    fun getPlay(id: Long): ResponseObject<PlayView> {
         val play = playRepository.findById(id)
 
-        if (play.isPresent) {
-            val playView = play.get().toView()
-            val writerSet = playWriterRelationRepository.getAllByPlayId(id).map { it.writerId }
-
-            playView.writers.addAll(writerRepository.findAllById(writerSet))
-
-            return ResponseEntity(playView, HttpStatus.OK)
+        if (!play.isPresent) {
+            throw NotFoundException("Play with $id not found")
         }
-        return ResponseEntity(HttpStatus.NOT_FOUND)
+
+        val playView = play.get().toView()
+        val writerSet = playWriterRelationRepository.getAllByPlayId(id).map { it.writerId }
+
+        playView.writers.addAll(writerRepository.findAllById(writerSet))
+
+        return ResponseObject(playView, HttpStatus.OK.value())
     }
 
-    fun editPlay(id: Long, playDto: PlayDto): ResponseEntity<Play> {
+    fun getWriters(id: Long): ResponseObject<List<WriterView>> {
+        if (!playRepository.existsById(id)) {
+            throw IllegalArgumentException("Play with id $id doesn't exist!")
+        }
+
+        val writers = playWriterRelationRepository.getAllByPlayId(id).map {
+            writerRepository
+                .findById(it.writerId)
+                .get()
+                .toView()
+        }
+
+        return ResponseObject(writers, HttpStatus.OK.value())
+    }
+
+    fun editPlay(id: Long, playDto: PlayDto): ResponseObject<Play> {
         val play = try {
             playRepository.getReferenceById(id)
         } catch (e: EmptyResultDataAccessException) {
-            return ResponseEntity(HttpStatus.BAD_REQUEST)
+            throw e
         }
-        return ResponseEntity(playRepository.save(play.update(playDto)), HttpStatus.OK)
+        return ResponseObject(playRepository.save(play.update(playDto)), HttpStatus.OK.value())
     }
 
-    fun deletePlay(id: Long): ResponseEntity<Play> {
+    fun deletePlay(id: Long): ResponseObject<String> {
         try {
             playRepository.deleteById(id)
         } catch (e: EmptyResultDataAccessException) {
-            return ResponseEntity(HttpStatus.BAD_REQUEST)
+            throw e
         }
-        return ResponseEntity(HttpStatus.OK)
+
+        return ResponseObject("Deleted successfully!", HttpStatus.OK.value())
     }
 }
--- src/main/kotlin/ru/theatrebel/service/WriterService.kt
+++ src/main/kotlin/ru/theatrebel/service/WriterService.kt
@@ -1,15 +1,17 @@
 package ru.theatrebel.service
 
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import ru.theatrebel.dto.WriterDto
 import ru.theatrebel.dto.toEntity
 import ru.theatrebel.entity.Writer
 import ru.theatrebel.entity.toView
+import ru.theatrebel.exception.NotFoundException
+import ru.theatrebel.exception.ValidateNullException
 import ru.theatrebel.repository.PlayRepository
 import ru.theatrebel.repository.PlayWriterRelationRepository
 import ru.theatrebel.repository.WriterRepository
+import ru.theatrebel.view.ResponseObject
 import ru.theatrebel.view.WriterView
 
 @Service
@@ -17,21 +19,30 @@ class WriterService(private val writerRepository: WriterRepository,
                     private val playRepository: PlayRepository,
                     private val playWriterRelationRepository: PlayWriterRelationRepository) {
 
-    fun addWriter(writerDto: WriterDto): Writer = writerRepository.save(writerDto.toEntity())
+    fun addWriter(writerDto: WriterDto): ResponseObject<Writer> {
+        val writer = try {
+            writerRepository.save(writerDto.toEntity())
+        } catch (e: ValidateNullException) {
+            throw e
+        }
 
-    fun getWriterById(id: Long): ResponseEntity<WriterView> {
+        return ResponseObject(writer, HttpStatus.OK.value())
+    }
+
+    fun getWriterById(id: Long): ResponseObject<WriterView> {
         val writer = writerRepository.findById(id)
 
-        if (writer.isPresent) {
-            val writerView = writer.get().toView()
-            val playSet = playWriterRelationRepository
-                .getAllByWriterId(id)
-                .map { it.playId }
-
-            writerView.plays.addAll(playRepository.findAllById(playSet))
-
-            return ResponseEntity(writerView, HttpStatus.OK)
+        if (!writer.isPresent) {
+            throw NotFoundException("Writer with $id not found")
         }
-        return ResponseEntity(HttpStatus.NOT_FOUND)
+
+        val writerView = writer.get().toView()
+        val playSet = playWriterRelationRepository
+            .getAllByWriterId(id)
+            .map { it.playId }
+
+        writerView.plays.addAll(playRepository.findAllById(playSet))
+
+        return ResponseObject(writerView, HttpStatus.OK.value())
     }
 }
