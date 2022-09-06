479c31954e3730d404bc8bc7ace7ef7f0ec9dbd6
bee-joo <bukin-danila@yandex.ru>
2022-09-06 22:08:20 +0300

    Rm

--- src/main/kotlin/ru/theatrebel/view/ResponseObject.kt
+++
@@ -1,7 +0,0 @@
-package ru.theatrebel.view
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class ResponseObject<T>(
-    @JsonProperty val message: T,
-    @JsonProperty val status: Int)
