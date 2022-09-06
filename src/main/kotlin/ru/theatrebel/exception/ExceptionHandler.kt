17216bff058f7036814b227f31f9446c2eb6a7ec
bee-joo <bukin-danila@yandex.ru>
2022-09-06 22:04:56 +0300

    Remove ResponseObject

--- src/main/kotlin/ru/theatrebel/exception/ExceptionHandler.kt
+++ src/main/kotlin/ru/theatrebel/exception/ExceptionHandler.kt
@@ -2,36 +2,29 @@ package ru.theatrebel.exception
 
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-import ru.theatrebel.view.ResponseObject
 
 @RestControllerAdvice
 class ExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(EmptyResultDataAccessException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleEmptyResultDataAccessException(e: EmptyResultDataAccessException): ResponseObject<String> {
-        return ResponseObject("Invalid data: ${e.message}", HttpStatus.BAD_REQUEST.value())
-    }
+    fun handleEmptyResultDataAccessException(e: EmptyResultDataAccessException) = ResponseEntity.badRequest().body("Invalid data: ${e.message}")
 
     @ExceptionHandler(IllegalArgumentException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseObject<String> {
-        return ResponseObject("Invalid data: ${e.message}", HttpStatus.BAD_REQUEST.value())
-    }
+    fun handleIllegalArgumentException(e: IllegalArgumentException) = ResponseEntity.badRequest().body("Invalid data: ${e.message}")
 
-    @ExceptionHandler(ValidateNullException::class)
+    @ExceptionHandler(ValidationException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleValidateNullException(e: ValidateNullException): ResponseObject<String> {
-        return ResponseObject(e.message, HttpStatus.BAD_REQUEST.value())
-    }
+    fun handleValidateNullException(e: ValidationException) = ResponseEntity.badRequest().body("Invalid data: ${e.message}")
 
     @ExceptionHandler(NotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleNotFoundException(e: NotFoundException): ResponseObject<String> {
-        return ResponseObject(e.message, HttpStatus.NOT_FOUND.value())
-    }
+    fun handleNotFoundException(e: NotFoundException) = ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found: ${e.message}")
+
 }
