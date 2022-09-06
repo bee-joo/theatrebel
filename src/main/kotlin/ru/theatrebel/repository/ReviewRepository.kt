4cb74553e1c83d9da71808add534f3aabe7b02cd
bee-joo <bukin-danila@yandex.ru>
2022-09-06 22:10:20 +0300

    New findAllByPlayId method

--- src/main/kotlin/ru/theatrebel/repository/ReviewRepository.kt
+++ src/main/kotlin/ru/theatrebel/repository/ReviewRepository.kt
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import ru.theatrebel.entity.Review
 
 interface ReviewRepository : JpaRepository<Review, Long> {
+    fun findAllByPlayId(id: Long): Set<Review>
 }
