33de0937359d6518eac8f7a08fe06494854f9fe2
bee-joo <bukin-danila@yandex.ru>
2022-10-27 15:09:06 +0300

    Add methods

--- src/main/kotlin/ru/theatrebel/service/WriterService.kt
+++ src/main/kotlin/ru/theatrebel/service/WriterService.kt
@@ -3,10 +3,14 @@ package ru.theatrebel.service
 import ru.theatrebel.dto.WriterDto
 import ru.theatrebel.entity.Writer
 import ru.theatrebel.view.PlayView
+import ru.theatrebel.view.ResponseObject
 import ru.theatrebel.view.WriterView
 
 interface WriterService {
     fun addWriter(writerDto: WriterDto): Writer
+    fun getAllWriters(): List<WriterView>
     fun getPlaysByWriterId(id: Long): List<PlayView>
     fun getWriterById(id: Long): WriterView
+    fun editWriter(id: Long, writerDto: WriterDto): Writer
+    fun deleteWriter(id: Long): ResponseObject<String>
 }
