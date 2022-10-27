806eca1f2209e86b3590b7ed1f11c7afcf08b332
bee-joo <bukin-danila@yandex.ru>
2022-10-27 15:08:00 +0300

    Set to List

--- src/main/kotlin/ru/theatrebel/view/PlayView.kt
+++ src/main/kotlin/ru/theatrebel/view/PlayView.kt
@@ -1,14 +1,15 @@
 package ru.theatrebel.view
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import ru.theatrebel.entity.Writer
+import ru.theatrebel.entity.Review
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class PlayView(
     val id: Long,
     val name: String,
-    var writers: MutableSet<Writer> = mutableSetOf(),
+    var writers: List<WriterView>? = null,
     var origname: String? = null,
     var date: Int? = null,
-    var description: String? = null
+    var description: String? = null,
+    var reviews: List<Review>? = null
 )
--- src/main/kotlin/ru/theatrebel/view/WriterView.kt
+++ src/main/kotlin/ru/theatrebel/view/WriterView.kt
@@ -1,13 +1,12 @@
 package ru.theatrebel.view
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import ru.theatrebel.entity.Play
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class WriterView(
     val id: Long,
     val name: String,
-    val plays: MutableSet<Play> = mutableSetOf(),
+    var plays: List<PlayView> = listOf(),
     var country: String? = null,
     var city: String? = null
 )
