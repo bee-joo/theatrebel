eb0c3af86177e20b5cd9ec862a8d403e34ac4219
bee-joo <bukin-danila@yandex.ru>
2022-10-27 15:11:22 +0300

    Change signature

--- src/main/kotlin/ru/theatrebel/service/ReviewService.kt
+++ src/main/kotlin/ru/theatrebel/service/ReviewService.kt
@@ -2,10 +2,11 @@ package ru.theatrebel.service
 
 import ru.theatrebel.dto.ReviewDto
 import ru.theatrebel.entity.Review
+import ru.theatrebel.view.ResponseObject
 
 interface ReviewService {
     fun getReview(id: Long): Review
     fun postReview(reviewDto: ReviewDto): Review
-    fun deleteReview(id: Long)
+    fun deleteReview(id: Long): ResponseObject<String>
     fun editReview(id: Long, reviewDto: ReviewDto): Review
 }
