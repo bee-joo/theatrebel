fa7a9c2fcd1d2c090b1489e202cc58a060b4b129
bee-joo <bukin-danila@yandex.ru>
2022-10-27 15:09:38 +0300

    Improve mapping objects

--- src/main/kotlin/ru/theatrebel/service/impl/WriterServiceImpl.kt
+++ src/main/kotlin/ru/theatrebel/service/impl/WriterServiceImpl.kt
@@ -1,44 +1,69 @@
 package ru.theatrebel.service.impl
 
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import ru.theatrebel.dto.WriterDto
 import ru.theatrebel.dto.toEntity
 import ru.theatrebel.entity.Writer
 import ru.theatrebel.entity.toView
+import ru.theatrebel.entity.update
 import ru.theatrebel.exception.NotFoundException
 import ru.theatrebel.exception.ValidationException
+import ru.theatrebel.repository.PlayRepository
 import ru.theatrebel.repository.PlayWriterRelationRepository
 import ru.theatrebel.repository.WriterRepository
 import ru.theatrebel.service.WriterService
 import ru.theatrebel.view.PlayView
+import ru.theatrebel.view.ResponseObject
 import ru.theatrebel.view.WriterView
 
 @Service
 class WriterServiceImpl(private val writerRepository: WriterRepository,
+                        private val playRepository: PlayRepository,
                         private val playWriterRelationRepository: PlayWriterRelationRepository) : WriterService {
 
     override fun addWriter(writerDto: WriterDto): Writer {
-        val writer = try {
+        return try {
             writerRepository.save(writerDto.toEntity())
         } catch (e: ValidationException) {
             throw e
         }
+    }
 
-        return writer
+    override fun getAllWriters(): List<WriterView> {
+        return writerRepository
+                .findAll()
+                .map { writer -> writer.toView() }
     }
 
     override fun getPlaysByWriterId(id: Long): List<PlayView> {
-        return playWriterRelationRepository.getAllByWriterId(id).map { it.toView() }
+        val res = playWriterRelationRepository.getAllPlaysByWriterId(id)
+
+        return if (res.isEmpty())
+            throw NotFoundException("Writer with id $id doesn't have plays!")
+            else res.map { play ->  play.toView() }
     }
 
     override fun getWriterById(id: Long): WriterView {
         val writer = writerRepository.findById(id).orElseThrow { NotFoundException("Writer with $id not found") }
+        val playList = playWriterRelationRepository.getAllPlaysByWriterId(id)
 
-        val writerView = writer.toView()
-        val playList = playWriterRelationRepository.getAllByWriterId(id)
+        return writer.toView(playList)
+    }
 
-        writerView.plays.addAll(playList)
+    override fun editWriter(id: Long, writerDto: WriterDto): Writer {
+        val writer = writerRepository.findById(id).orElseThrow { NotFoundException("Writer with $id not found") }
 
-        return writerView
+        return writerRepository.save(writer.update(writerDto))
+    }
+
+    override fun deleteWriter(id: Long): ResponseObject<String> {
+        try {
+            writerRepository.deleteById(id)
+            return ResponseObject(HttpStatus.OK.value(), "Deleted")
+        } catch (e: EmptyResultDataAccessException) {
+            throw e
+        }
     }
 }
