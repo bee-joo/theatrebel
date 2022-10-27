90f523b8897da8283e8f7bf0764272409d28eea9
bee-joo <bukin-danila@yandex.ru>
2022-10-27 15:08:57 +0300

    Change signature

--- src/main/kotlin/ru/theatrebel/service/PlayService.kt
+++ src/main/kotlin/ru/theatrebel/service/PlayService.kt
@@ -4,13 +4,16 @@ import ru.theatrebel.dto.PlayDto
 import ru.theatrebel.dto.ReviewDto
 import ru.theatrebel.entity.*
 import ru.theatrebel.view.PlayView
+import ru.theatrebel.view.ResponseObject
 import ru.theatrebel.view.WriterView
 
 interface PlayService {
     fun addPlay(playDto: PlayDto): Play
     fun getPlay(id: Long): PlayView
+    fun getAllPlays(): List<PlayView>
     fun getWriters(id: Long): List<WriterView>
     fun editPlay(id: Long, playDto: PlayDto): Play
-    fun deletePlay(id: Long)
+    fun deletePlay(id: Long): ResponseObject<String>
     fun addReview(id: Long, reviewDto: ReviewDto): Review
+    fun getReviews(id: Long): List<Review>
 }
