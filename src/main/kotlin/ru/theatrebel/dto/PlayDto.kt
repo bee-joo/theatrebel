a590bde4f0fd8a7ed9495ce6b087d1e41142b921
bee-joo <bukin-danila@yandex.ru>
2023-01-12 16:53:52 +0300

    New columns

--- src/main/kotlin/ru/theatrebel/dto/PlayDto.kt
+++ src/main/kotlin/ru/theatrebel/dto/PlayDto.kt
@@ -1,21 +1,11 @@
 package ru.theatrebel.dto
 
-import ru.theatrebel.entity.Play
-import ru.theatrebel.exception.ValidationException
-
 data class PlayDto(
     val name: String? = null,
     val writerIds: List<Long>? = null,
     val origname: String? = null,
     val date: Int? = null,
-    val description: String? = null
+    val description: String? = null,
+    val city: String? = null,
+    val text: String? = null
 )
-
-fun PlayDto.toEntity(): Play {
-    val play = Play(this.name ?: throw ValidationException("Name excepted"))
-    this.origname?.let { play.origname = it }
-    this.date?.let { play.date = it }
-    this.description?.let { play.description = it }
-
-    return play
-}
