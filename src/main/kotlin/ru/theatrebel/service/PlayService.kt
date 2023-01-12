2651171b175ef1fa65b7bec464a6619654963a88
bee-joo <bukin-danila@yandex.ru>
2023-01-12 16:54:54 +0300

    New params from query

--- src/main/kotlin/ru/theatrebel/service/PlayService.kt
+++ src/main/kotlin/ru/theatrebel/service/PlayService.kt
@@ -1,17 +1,19 @@
 package ru.theatrebel.service
 
+import org.springframework.data.domain.Page
 import ru.theatrebel.dto.PlayDto
 import ru.theatrebel.dto.ReviewDto
+import ru.theatrebel.dto.request.GetAllPlaysRequest
 import ru.theatrebel.entity.*
-import ru.theatrebel.view.PlayView
-import ru.theatrebel.view.ResponseObject
-import ru.theatrebel.view.WriterView
+import ru.theatrebel.dto.view.PlayView
+import ru.theatrebel.dto.view.ResponseObject
+import ru.theatrebel.dto.view.WriterView
 
 interface PlayService {
     fun addPlay(playDto: PlayDto): Play
     fun getPlay(id: Long): PlayView
-    fun getAllPlays(): List<PlayView>
-    fun getWriters(id: Long): List<WriterView>
+    fun getAllPlays(request: GetAllPlaysRequest): Page<PlayView>
+    fun getWriters(id: Long, orderBy: String, page: String, count: String): Page<WriterView>
     fun editPlay(id: Long, playDto: PlayDto): Play
     fun deletePlay(id: Long): ResponseObject<String>
     fun addReview(id: Long, reviewDto: ReviewDto): Review
