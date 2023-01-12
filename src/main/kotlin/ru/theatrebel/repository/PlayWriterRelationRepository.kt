a5833dfcdce2f45119280ed940030d749782d3c1
bee-joo <bukin-danila@yandex.ru>
2023-01-12 16:56:08 +0300

    New queries

--- src/main/kotlin/ru/theatrebel/repository/PlayWriterRelationRepository.kt
+++ src/main/kotlin/ru/theatrebel/repository/PlayWriterRelationRepository.kt
@@ -1,5 +1,7 @@
 package ru.theatrebel.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import ru.theatrebel.entity.Play
@@ -12,6 +14,12 @@ interface PlayWriterRelationRepository : CrudRepository<PlayWriterRelation, UUID
     @Query(value = "SELECT w FROM Writer w, PlayWriterRelation pw WHERE pw.playId = ?1 AND w.id = pw.writerId")
     fun getAllWritersByPlayId(playId: Long): List<Writer>
 
+    @Query(value = "SELECT w FROM Writer w, PlayWriterRelation pw WHERE pw.playId = ?1 AND w.id = pw.writerId")
+    fun getAllWritersByPlayId(playId: Long, pageable: Pageable): Page<Writer>
+
     @Query(value = "SELECT p from Play p, PlayWriterRelation pw WHERE pw.writerId = ?1 AND p.id = pw.playId")
     fun getAllPlaysByWriterId(writerId: Long): List<Play>
+
+    @Query(value = "SELECT p from Play p, PlayWriterRelation pw WHERE pw.writerId = ?1 AND p.id = pw.playId")
+    fun getAllPlaysByWriterId(writerId: Long, pageable: Pageable): Page<Play>
 }
