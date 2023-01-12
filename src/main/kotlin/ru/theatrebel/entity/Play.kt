5048de5003da2e09c137c989500eda347532cdbf
bee-joo <bukin-danila@yandex.ru>
2023-01-12 16:53:24 +0300

    New columns

--- src/main/kotlin/ru/theatrebel/entity/Play.kt
+++ src/main/kotlin/ru/theatrebel/entity/Play.kt
@@ -2,9 +2,8 @@ package ru.theatrebel.entity
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
-import ru.theatrebel.dto.PlayDto
-import ru.theatrebel.view.PlayView
 import javax.persistence.*
+import javax.validation.constraints.NotNull
 
 @Entity
 @Table(name = "plays")
@@ -20,37 +19,12 @@ class Play(
     @Column(length = 2000)
     var description: String? = null,
 
+    var city: String? = null,
+    var text: String? = null,
+    @NotNull var hasText: Boolean? = false,
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     var id: Long? = null
 )
-
-fun Play.toView(
-    writers: List<Writer> = emptyList(),
-    reviews: List<Review> = emptyList()
-): PlayView {
-    val playView = PlayView(this.id!!, this.name)
-
-    this.origname?.let { playView.origname = it }
-    this.date?.let { playView.date = it }
-    this.description?.let { playView.description = it }
-
-    if (writers.isNotEmpty()) {
-        playView.writers = writers.map { writer -> writer.toView() }
-    }
-    if (reviews.isNotEmpty()) {
-        playView.reviews = reviews
-    }
-
-    return playView
-}
-
-fun Play.update(playDto: PlayDto): Play {
-    playDto.name?.let { this.name = it }
-    playDto.origname?.let { this.origname = it }
-    playDto.date?.let { this.date = it }
-    playDto.description?.let { this.description = it }
-
-    return this
-}
