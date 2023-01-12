d5e0a92cd9aeca11699047a419f77c515ab15c8e
bee-joo <bukin-danila@yandex.ru>
2023-01-12 16:55:23 +0300

    ExampleMatcher for getAllPlays service function

--- src/main/kotlin/ru/theatrebel/service/impl/PlayServiceImpl.kt
+++ src/main/kotlin/ru/theatrebel/service/impl/PlayServiceImpl.kt
@@ -1,22 +1,29 @@
 package ru.theatrebel.service.impl
 
 import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.data.domain.Example
+import org.springframework.data.domain.ExampleMatcher
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import ru.theatrebel.dto.PlayDto
 import ru.theatrebel.dto.ReviewDto
+import ru.theatrebel.dto.request.GetAllPlaysRequest
 import ru.theatrebel.dto.toEntity
 import ru.theatrebel.entity.*
 import ru.theatrebel.exception.NotFoundException
 import ru.theatrebel.exception.ValidationException
+import ru.theatrebel.mapper.*
 import ru.theatrebel.repository.PlayRepository
 import ru.theatrebel.repository.PlayWriterRelationRepository
 import ru.theatrebel.repository.ReviewRepository
 import ru.theatrebel.repository.WriterRepository
 import ru.theatrebel.service.PlayService
-import ru.theatrebel.view.PlayView
-import ru.theatrebel.view.ResponseObject
-import ru.theatrebel.view.WriterView
+import ru.theatrebel.dto.view.PlayView
+import ru.theatrebel.dto.view.ResponseObject
+import ru.theatrebel.dto.view.WriterView
 
 @Service
 class PlayServiceImpl(private val playRepository: PlayRepository,
@@ -50,17 +57,32 @@ class PlayServiceImpl(private val playRepository: PlayRepository,
         )
     }
 
-    override fun getAllPlays(): List<PlayView> {
-        return playRepository.findAll()
+    override fun getAllPlays(request: GetAllPlaysRequest): Page<PlayView> {
+        val matcher = ExampleMatcher.matching()
+            .withMatcher("date", ExampleMatcher.GenericPropertyMatcher().exact())
+            .withMatcher("hasText", ExampleMatcher.GenericPropertyMatcher().exact())
+            .withIgnorePaths("name")
+            .withIgnoreNullValues()
+
+        val playMatcher = Play(
+            name = "",
+            hasText = request.hasText,
+            date = request.year?.toInt()
+        )
+
+        return playRepository
+            .findAll(Example.of(playMatcher, matcher), PageRequest.of(request.page.toInt(), request.count.toInt()))
             .map { play -> play.toView(playWriterRelationRepository.getAllWritersByPlayId(play.id!!)) }
     }
 
-    override fun getWriters(id: Long): List<WriterView> {
+    override fun getWriters(id: Long, orderBy: String, page: String, count: String): Page<WriterView> {
         if (!playRepository.existsById(id)) {
             throw NotFoundException("Play with id $id doesn't exist!")
         }
 
-        return playWriterRelationRepository.getAllWritersByPlayId(id).map { it.toView()}
+        return playWriterRelationRepository
+            .getAllWritersByPlayId(id, PageRequest.of(page.toInt(), count.toInt(), Sort.by(orderBy)))
+            .map { it.toView()}
     }
 
     override fun editPlay(id: Long, playDto: PlayDto): Play {
